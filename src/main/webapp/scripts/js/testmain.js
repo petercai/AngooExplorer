28aca4796348ba4dc4a137bdccfacc991c8d8112
Peter Cai <petercaica@hotmail.com>
2012-09-08 00:48:41 -0400

    layout work

--- src/main/webapp/scripts/js/testmain.js
+++ src/main/webapp/scripts/js/testmain.js
@@ -1,58 +1,37 @@
-// Ext.ns('age.TestApp');
-// 
-// age.TestApp = function(){
-	// return {
-		// init: function(){
-            // Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
-            // Ext.QuickTips.init();
-            // // Ext.BLANK_IMAGE_URL = '../../resources/images/default/s.gif';
-            // // Ext.Direct.addProvider(Imgorg.REMOTING_API);
-// //             
-            // // Ext.ux.SwfuMgr.on('filequeued', this.onFileQueued, this);
-// //             
-// 
-//             
-            // // tabPanel = Ext.getCmp('img-tabpanel');
-            // // thumbPanel = tabPanel.getComponent('images-view');
-            // // Imgorg.TagWin = new Imgorg.TagWindow();
-// 
-		// }
-	// }
-// }();
-Ext.onReady(function(){
+Ext.ns('age.TestApp');
 	
+age.TestApp = function(){
+	return {
+		init: function(){
+            Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+            Ext.QuickTips.init();
             new Ext.Viewport({
                 layout:'border',
-                items: [{
-                    xtype: 'treepanel',
-                    id: 'album-tree',
+                items:[
+                {
+                    region: 'north',
+                    contentEl: 'ext_header',
+                    width: '100%'
+                },
+                {
+                    /*
+                     * TODO: TREE PANEL
+                     */
+                    xtype: 'panel',
                     region: 'west',
-                    width: 180,
-                    minWidth: 180,
-                    maxWidth: 180,
-                    collapsible: true,
-                    split: true,
-                    collapseMode: 'mini',
-                    margins: '5 0 5 5',
-                    cmargins: '0 0 0 0',
-                    tbar: [{
-                        text: 'Add Album',
-                        iconCls: 'add',
-                        scale: 'large',
-                        handler: function() {
-                        }
-                    },{
-                        text: 'Upload',
-                        iconCls: 'upload',
-                        scale: 'large',
-                        handler: function() {
-                        }
-                    }]
-                },{
+                    width: 150,
+                    html: 'West Panel'
+                },
+                {
                     xtype: 'panel',
                     region: 'center',
-                    id: 'img-tabpanel',
-                    margins: '5 5 5 0'
+                    html: 'Center panel',
+                    margins: '0 0 0 0'
                 }]
             });
-});
+		}
+	}
+}();
+
+
+Ext.onReady(age.TestApp.init,age.TestApp);
